ea1bf1fabb143cf0c63dc7b44c45a5bc4c071275
unknown <yassindahroug1892006@gmail.com>
2026-08-16 07:56:12 +0300

    Solve following distance monitor challenge

--- Coding_Assignment/Question.py
+++ Coding_Assignment/Question.py
@@ -1,15 +1,29 @@
 def monitor_following_distance(distances: list[float], speeds: list[float]) -> tuple[int, float, int]:
-    """
-    Analyzes following distance compared to safe distance (speed * 0.5).
-    
-    Args:
-        distances (list[float]): Distance to the lead car at each second.
-        speeds (list[float]): Speed of our car at each second.
-        
-    Returns:
-        tuple[int, float, int]: (tailgating_seconds, minimum_distance, tailgate_incidents)
-            - tailgating_seconds: total seconds distance was < safe distance
-            - minimum_distance: absolute closest distance to the lead car (return 0.0 if empty list)
-            - tailgate_incidents: number of separate instances the car started tailgating
-    """
-    pass
+
+ if len(distances) == 0 :
+  return (0, 0.0, 0)
+
+ min = distances[0] 
+ was_tailgating = False 
+ now_tailgating = False 
+ tailgating_seconds = 0 
+ incident_counter = 0
+
+ for i in range(len(distances)) :
+  safe_distance = speeds[i] * 0.5
+ 
+  if distances[i] < safe_distance :
+   tailgating_seconds+=1
+   now_tailgating = True
+
+  if distances[i] < min :
+   min = distances[i]
+
+  if now_tailgating == True and was_tailgating == False :
+   incident_counter+=1
+
+  was_tailgating = now_tailgating 
+  now_tailgating = False
+
+ return(tailgating_seconds, min, incident_counter)
+
